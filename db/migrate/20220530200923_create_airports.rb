aa1cbb186a7f40c50f28d653dc3a139717e0742c
Jason Watkins <goushoryuken@gmail.com>
2022-06-06 17:40:38 -0700

    heroku db change

--- db/migrate/20220530200923_create_airports.rb
+++ db/migrate/20220530200923_create_airports.rb
@@ -1,6 +1,8 @@
 class CreateAirports < ActiveRecord::Migration[7.0]
   def change
     create_table :airports do |t|
+      t.string :name
+      t.string :symbol
 
       t.timestamps
     end
--- db/migrate/20220531170910_add_fields_to_airport.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsToAirport < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :flights, :origin, index: true
-    add_foreign_key :flights, :airports, column: :origin_id
-    add_reference :flights, :destination, index: true
-    add_foreign_key :flights, :airports, column: :destination_id
-  end
-end
