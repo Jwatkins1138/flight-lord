478e4ae6565b5b10809e850fa9ce359bb2c164bc
Jason Watkins <goushoryuken@gmail.com>
2022-06-06 17:22:03 -0700

    6

--- db/migrate/20220531175517_add_airport_to_flight.rb
+++ db/migrate/20220531175517_add_airport_to_flight.rb
@@ -1,15 +1,10 @@
 class AddAirportToFlight < ActiveRecord::Migration[7.0]
   def change
-    # add_reference :flights, :origin
-    # , index: true
-    # add_foreign_key :flights, :airports, column: :origin_id
-    # add_reference :flights, :destination
-    # , index: true
-    # add_foreign_key :flights, :airports, column: :destination_id
+    add_reference :flights, :origin, index: true
+    add_foreign_key :flights, :airports, column: :origin_id
+    add_reference :flights, :destination, index: true
+    add_foreign_key :flights, :airports, column: :destination_id
   end
-  def change
-    add_column :airports, :name, :string
-    add_column :airports, :symbol, :string
-  end
-  
+ 
+
 end
--- db/migrate/20220606234005_add_name_to_airport.rb
+++
@@ -1,8 +0,0 @@
-class AddNameToAirport < ActiveRecord::Migration[7.0]
-  def change
-    def change
-      add_column :airports, :name, :string
-      add_column :airports, :symbol, :string
-    end
-  end
-end
